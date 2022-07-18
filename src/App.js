f796c704182d5649d01d0bf4b7d390b571c6588c
Pedro Paulo <pedropaulodaris@gmail.com>
2022-07-18 03:07:41 -0300

    containers + hooks and screen done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GlobalStyle from './assets/GlobalStyle';
+import Home from './screen/Home';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Login from './screen/Login';
+import ProtectedRoute from './components/helper/ProtectedRoute';
+import { UserStorage } from './UserContext';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <>
+      <HashRouter>
+        <UserStorage>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <ProtectedRoute path="/home" element={<Home />} />
+          </Routes>
+        </UserStorage>
+      </HashRouter>
+    </>
+  );
+};
 
 export default App;
